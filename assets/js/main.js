329c28a19eda300adb0358a87aef9f0dc4d82efe
Le Cuong <cuonglt1408@gmail.com>
2020-04-03 18:12:35 +0700

    add menu mobile

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,8 +7,8 @@ miniCart.forEach(item => {
   item.addEventListener("click", function(e) {
     e.preventDefault();
     e.stopPropagation();
-    var id = item.attributes.href.value;
-    var target = document.querySelector(id);
+    let id = item.attributes.href.value;
+    let target = document.querySelector(id);
     target.classList.toggle("open");
     overlay.classList.add("overlay-open");
   });
@@ -17,18 +17,51 @@ miniCart.forEach(item => {
 /*----------------------------------------*/
 /*  Close Button Actions
 /*----------------------------------------*/
-function getParent(parent) {
-  for (let x in parent) {
+function getParent(parent, className) {
+  while (!parent.matches(className)) {
     parent = parent.parentElement;
-    if (parent.matches(".open")) return parent;
   }
+  return parent;
 }
+
 let btnClose = document.querySelectorAll(".btn-close");
 btnClose.forEach(btn => {
   btn.addEventListener("click", function(e) {
     e.preventDefault();
-    var target = getParent(this);
+    let target = getParent(this, ".open");
     target.classList.remove("open");
     overlay.classList.remove("overlay-open");
+  });
+});
+
+/*  Offcanvas
+/*----------------------------------------*/
+/*Variables*/
+let offcanvasNav = document.querySelectorAll(".mobile-menu");
+let offcanvasNavWrap = document.querySelectorAll(".mobile-menu_wrapper");
+let offcanvasNavSubMenu = document.querySelectorAll(".mobile-menu .sub-menu");
+
+function checkClass(item, className) {
+  for (x of item) {
+    if (x.classList.contains(className)) return x;
+  }
+}
+
+offcanvasNavSubMenu.forEach(item => {
+  let parent = item.parentElement;
+  let newItem = document.createElement("span");
+  newItem.className = "menu-expand";
+  newItem.innerHTML = '<i class="ion-ios-plus-empty"></i>';
+  parent.appendChild(newItem);
+  item.classList.add("hide");
+});
+
+offcanvasNav.forEach(item => {
+  item.addEventListener("click", e => {
+    let itemTarget = e.target;
+    let itemParent = getParent(itemTarget, ".menu-item-has-children");
+    let getSubMenu = checkClass(itemParent.children, "sub-menu");
+    itemParent.classList.toggle("active");
+    getSubMenu.classList.toggle("toggle-hide");
   });
 });
