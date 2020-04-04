7f99f289993210383854a3a2485fcb5e1fd55d00
Le Cuong <cuonglt1408@gmail.com>
2020-04-04 18:35:53 +0700

    add product slider

--- assets/js/main.js
+++ assets/js/main.js
@@ -65,13 +65,45 @@ offcanvasNav.forEach(item => {
     getSubMenu.classList.toggle("toggle-hide");
   });
 });
-
-$('.home-slider').slick({
+$(".home-slider").slick({
   autoplay: true,
   infinite: true,
   slidesToShow: 1,
   slidesToScroll: 1,
   dots: true,
-  dotsClass: 'slick-dots',
-  fade: true,
+  dotsClass: "slick-dots",
+  fade: true
+});
+
+$(document).ready(function() {
+  $(".shella-element-carousel").slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    infinite: true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
 });
