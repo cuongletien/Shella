1b17939ecb935c53a4aa74b78689136d85cd7154
Le Cuong <cuonglt1408@gmail.com>
2020-04-04 20:20:40 +0700

    add brand

--- assets/js/main.js
+++ assets/js/main.js
@@ -131,3 +131,34 @@ $(".latest-blog_slider").slick({
     }
   ]
 });
+$(".brand-slider").slick({
+  slidesToShow: 6,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  arrows: false,
+  infinite: true,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 4,
+        slidesToScroll: 4
+      }
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
+});
